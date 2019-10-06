3be4fe1e4d42108538956d53335b12e31b5a65b4
BarKench <wagnercandido10123@gmail.com>
2019-10-06 20:31:44 -0300

    06.10.2019

---
+++ site/sobre.php
@@ -0,0 +1,48 @@
+<?php
+include "anterior.php";
+?>
+<h1 class="text-center mt-3"></h1>
+<div class="row">
+
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="newspaper">
+                <h2>Sobre</h2>
+					<p>Bem-vindo à ShiningHouse, sua opção número um quando se trata de divulgação. Nos dedicamos a oferecer a melhor plataforma de divulgação, com foco em gratuidade e comunicação direta com o prestrador do serviço.<p>
+
+
+<p>Desenvolvida em 2019, a ShiningHouse desenvolveu-se em um pouco mais de um mês e estamos entusiasmados por mostrar o resultado do projeto.</p>
+
+
+<p> Esperamos que você goste da plataforma tanto quanto nós gostamos e tentamos oferecê-los a você. Se você tiver alguma dúvida ou comentário, não hesite em entrar em contato conosco</p>
+
+
+Atenciosamente,
+
+Equipe de desenvolvimento da plataforma.
+            </div>
+        </div>
+
+    </body>
+    <?php
+    include "posterior.php";
+    ?>
+
+    <style>
+        .newspaper {
+            /* Old Chrome, Safari and Opera */
+            text-align: justify;
+            -webkit-column-count: 2;
+            -webkit-column-gap: 100px;
+            -webkit-column-rule: 1px solid lightblue;
+
+        }
+
+        h2 {
+            -webkit-column-span: all;
+            /* Chrome, Safari, Opera */
+            column-span: all;
+        }
+    </style>
